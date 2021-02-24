1740011a104e29d197f0bca9f723d107156aa2a7
Valerio <valeriodevivo90@gmail.com>
2021-02-24 16:08:04 +0100

    Finish

--- index.php
+++ index.php
@@ -7,6 +7,19 @@
     <title>php-badwords</title>
 </head>
 <body>
-    ciao
+    <?php
+        $frase = 'Lorem ipsum dolor sit amet consectetur adipisicing elit Assumenda earum';
+
+        $parola = $_GET["parola"];
+        $lung = strlen($frase);
+        $bad = str_replace($parola, '***' , $frase);
+
+
+        echo 'la frase è: ' .$frase .'<br>';
+        echo 'la frase è lunga: ' .$lung .'<br>';
+        echo 'la frase con badword: ' .$bad .'<br>';
+        echo 'la badword è:' .$parola;
+
+    ?>
 </body>
 </html>
